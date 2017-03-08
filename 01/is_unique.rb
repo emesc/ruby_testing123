2f7a6f85b10be935fd16a06603eb0469824deb6e
emesc <tahder77@gmail.com>
2017-03-08 19:33:44 +1100

    Updated problem statements.

--- 01/is_unique.rb
+++ 01/is_unique.rb
@@ -1,4 +1,5 @@
 # Implement an algorithm to determine if a string has all unique characters
+
 def is_unique?(string)
   string_hash = Hash.new(0)
 
--- 01/url-ify.rb
+++ 01/url-ify.rb
@@ -1,3 +1,6 @@
+# Implement an algorithm that replaces space character with "%20"
+
+# replaces consecutive spaces with a single "%20"
 def urlify(string)
   puts string.strip.gsub(/[ ]+/, "%20")
 end
