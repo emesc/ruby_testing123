6bef5a5a2a9458a6925f071805c39196c9a0f231
emesc <tahder77@gmail.com>
2017-05-15 14:47:05 +1000

    Just rewrote. No mods.

--- lib/01/is_unique.rb
+++ lib/01/is_unique.rb
@@ -1,12 +1,12 @@
 # Implement an algorithm to determine if a string has all unique characters
 
 def is_unique?(string)
-  string_hash = Hash.new(0)
+    string_hash = Hash.new(0)
 
-  string.length.times do |i|
-    string_hash[string[i]] += 1
-    return false if string_hash[string[i]] > 1
-  end
+    string.length.times do |i|
+      string_hash[string[i]] += 1
+      return false if string_hash[string[i]] > 1
+    end
 
-  return true
+    return true
 end
